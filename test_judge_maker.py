1259bb83e9774318e7a053b62a22d47aa15fa353
Shingai-220424 <OCA2204240009@edu.oca.ac.jp>
2024-10-26 17:29:36 +0900

    a

--- test_judge_maker.py
+++ test_judge_maker.py
@@ -4,7 +4,7 @@ def test_make_judge_no1():
     """マトリックスNo1
     10点より下の点数がある場合
     """
-    gradeList = ['A', 'B', 'C', 'D', 'E']
+    gradeList = ['A', 'B', 'C', 'D', 'E', 'G']
     pointsList = [
         [9, 100, 100, 100, 100, 100, 100, 100, 100, 100],
         [11, 21, 15, 100, 100, 100, 100, 100, 100, 100],
@@ -13,10 +13,10 @@ def test_make_judge_no1():
         [20, 100, 100, 100, 100, 100, 100, 100, 100, 30],
         [20, 100, 100, 100, 100, 100, 100, 100, 100, 30]
     ]
-    resultList = [3, 2, 1, 2, 3, 1]
+    resultList = [3, 2, 1, 2, 3, 2]
     try:
         for index, grade in enumerate(gradeList):
             result = make_judge(grade, pointsList[index])
             assert result == resultList[index]
     except Exception as e:
-        assert e.args[0] == 'エラーが発生しました。'
+        assert e.args[0] == 'gradeにA～E以外の文字が入力されています'
